3123a6d27d5558965dd2b1a350bd4fb14d16fb7b
spandios <wndudpower@gmail.com>
2022-11-23 12:33:48 +0900

    init

--- init/postgres-init.sql
+++ init/postgres-init.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE merge_main_db;
-ALTER USER postgres WITH PASSWORD 'dbMerge135!#%';
-GRANT ALL PRIVILEGES ON DATABASE "merge_main_db" to postgres;
+CREATE DATABASE test;
+ALTER USER postgres WITH PASSWORD 'postgres';
+GRANT ALL PRIVILEGES ON DATABASE "test" to postgres;
